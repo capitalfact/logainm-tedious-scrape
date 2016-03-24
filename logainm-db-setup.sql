b2318ad143fc0be8eef56e28a84b0140c3f4c6ee
capitalfact <seanpuddin@gmail.com>
2016-03-24 23:06:22 +0000

    Initial DB Schema

---
+++ logainm-db-setup.sql
@@ -0,0 +1,35 @@
+DROP DATABASE logainm;
+CREATE DATABASE logainm;
+USE logainm;
+
+CREATE TABLE place_name (
+	id INT NOT NULL AUTO_INCREMENT,
+	en VARCHAR(256) NOT NULL,
+	ga VARCHAR(256) NOT NULL,
+
+	PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE place_type (
+        id INT NOT NULL AUTO_INCREMENT,
+        code VARCHAR(64) NOT NULL UNIQUE,
+        description_en VARCHAR(256) NOT NULL,
+        description_ga VARCHAR(256) NOT NULL,
+
+        PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE place (
+	id INT NOT NULL AUTO_INCREMENT,
+	logainm_id INT NOT NULL,
+	place_name_id INT NOT NULL,
+	place_type_id INT NOT NULL,	
+	longitude FLOAT,
+	latitude FLOAT,
+	geo_accurate BOOLEAN NOT NULL DEFAULT FALSE,
+
+	PRIMARY KEY (id),
+	CONSTRAINT FOREIGN KEY (place_type_id) REFERENCES place_type (id),
+	CONSTRAINT FOREIGN KEY (place_name_id) REFERENCES place_name (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
