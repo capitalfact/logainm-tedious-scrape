e4b39224ad427982061ff7dfc81ddd740b292fb0
capitalfact <seanpuddin@gmail.com>
2016-03-27 22:22:37 +0100

    Implement fancy class

--- logainm-scrape.py
+++ logainm-scrape.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
 """Logainm Scraper"""
 
 import codecs
 import requests
-from lxml import etree
+from logainm import Logainm
 
+EN = 'en'
+GA = 'ga'
 BASE_URL = 'http://www.logainm.ie/xml/'
 
 PLACE_NAME_FILENAME = 'output/place_name.csv'
@@ -22,8 +23,6 @@ PLACE_TYPE_CSV.truncate()
 PLACE_CSV.truncate()
 PLACE_IS_IN_CSV.truncate()
 
-PARSER = etree.XMLParser(encoding='utf-8')
-
 PLACE_TYPES = set()
 
 PLACE_ID = 1
@@ -35,39 +34,35 @@ PLACE_TYPE_TO_ID = {}
 for i in range(2):
     response = requests.get(BASE_URL + str(i), headers={'Content-Type': 'application/xml'})
 
-    xml = etree.XML(response.content, PARSER)
+    place = Logainm(response)
 
-    non_existent_place = xml.xpath("//place[@nonexistent='yes']/source")
-
-    if non_existent_place:
+    if not place.exists():
         print BASE_URL + str(i) + ": INVALID"
     else:
         print "Processing: " + BASE_URL + str(i)
 
         # place_name
-        en_name = ""
-        ga_name = ""
-        en_names = xml.xpath("//name[@lang='en']")
-        for en in en_names:
-            if en.get('isMain') == "yes":
-                en_name = en.get('wording')
-        ga_names = xml.xpath("//name[@lang='ga']")
-        for ga in ga_names:
-            if ga.get('isMain') == "yes":
-                ga_name = ga.get('wording')
-        if en_name == "" or ga_name == "":
-            print "Problem with names"
+        en_name = place.get_main_name(EN)
+        ga_name = place.get_main_name(GA)
 
-        key = en_name + "/" + ga_name
+        if not en_name:
+            print "No name in English\n"
+        elif not ga_name:
+            print "Gan ainm as Gaeilge\n"
+
+        key = "%s/%s", en_name, ga_name
         PLACE_NAME_TO_ID[key] = PLACE_NAME_ID
-        PLACE_NAME_CSV.write(str(PLACE_NAME_ID) + "," + en_name + "," + ga_name + "\n")
+
+        # TODO move to csv module to fix this
+        # PLACE_NAME_CSV.write(("%s,%s,%s\n", str(PLACE_NAME_ID), en_name, ga_name))
         PLACE_NAME_ID += 1
 
         # place_type
-        place_type_obj = xml.xpath("//type")[0]
-        place_type_code = place_type_obj.get('id')
-        place_type_name_en = place_type_obj.get('titleEN')
-        place_type_name_ga = place_type_obj.get('titleGA')
+        type_element = place.getelement('type')
+
+        place_type_code = type_element.get('id')
+        place_type_name_en = type_element.get('titleEN')
+        place_type_name_ga = type_element.get('titleGA')
 
         if place_type_code not in PLACE_TYPE_TO_ID:
             PLACE_TYPE_TO_ID[place_type_code] = PLACE_TYPE_ID
@@ -81,25 +76,22 @@ for i in range(2):
 
         # place
         logainm_id = i
-        name_id = PLACE_NAME_TO_ID[en_name + "/" + ga_name]
+        name_id = PLACE_NAME_TO_ID["%s/%s", en_name, ga_name]
         type_id = PLACE_TYPE_TO_ID[place_type_code]
-        geos = xml.xpath("//geo")
-        lon = ""
-        lat = ""
+        geo = place.getelement('geo')
+        lon = geo.get('lon')
+        lat = geo.get('lat')
         geo_accurate = 0
-        if len(geos) > 0:
-            geo = xml.xpath("//geo")[0]
-            lon = geo.get('lon')
-            lat = geo.get('lat')
-            if geo.get('isAccurate') == "yes":
-                geo_accurate = 1
+        if geo.get('isAccurate') == 'yes':
+            geo_accurate = 1
 
         PLACE_CSV.write(str(PLACE_ID) + "," + str(logainm_id) + "," + str(name_id) + "," + str(
             type_id) + "," + lon + "," + lat + "," + str(geo_accurate) + "\n")
         PLACE_ID += 1
 
         # is in relationships
-        is_ins = xml.xpath("//isIn")
+
+        is_ins = place.getelement("isIn")
         for is_in in is_ins:
             belongs_to = is_in.get('placeID')
             PLACE_IS_IN_CSV.write(str(i) + "," + belongs_to + "\n")
