50f01d4286f3782cfb5c1c2b7d4fce2440bd6135
capitalfact <seanpuddin@gmail.com>
2016-03-28 09:46:41 +0100

    Use place objects

--- logainmparser.py
+++ logainmparser.py
@@ -1,5 +1,6 @@
 """Logainm data base scraping and parsing"""
 from lxml import etree
+from logainmdb import Place, PlaceName, PlaceType
 
 PARSER = etree.XMLParser(encoding='utf-8')
 
@@ -9,13 +10,52 @@ class LogainmParser:
         self.responsexml = etree.XML(response, PARSER)
 
     def getplace(self):
-        # return first place for now
-        return self.responsexml.xpath("//place")[0]
+        # get first place for now
+        placeobj = self.getallelements('place')[0]
+
+        logainm_id = placeobj.get('id')
+        place_name = self.getplacename()
+        place_type = self.getplacetype()
+
+        geo = placeobj.get('geo')
+        lon = None
+        lat = None
+        geo_accurate = False
+        if geo is not None and len(geo) > 0:
+            lon = geo.get('lon')
+            lat = geo.get('lat')
+            if geo.get('isAccurate') == 'yes':
+                geo_accurate = True
+        return Place(logainm_id, place_name, place_type, lon, lat, geo_accurate)
+
+    def getplacename(self):
+        wording_en = ''
+        wording_ga = ''
+        for name in self.getallelements("name"):
+            if name.get('isMain') == 'yes':
+                wording = name.get('wording')
+                lang = name.get('lang')
+                if lang == 'en':
+                    wording_en = wording
+                elif lang == 'ga':
+                    wording_ga = wording
+        return PlaceName(wording_en, wording_ga)
+
+    def getplacetype(self):
+        code = ''
+        name_en = ''
+        name_ga = ''
+        typeelems = self.getallelements('type')
+        if typeelems is not None and len(typeelems) > 0:
+            code = typeelems[0].get('id')
+            name_en = typeelems[0].get('titleEN')
+            name_ga = typeelems[0].get('titleGA')
+        return PlaceType(code, name_en, name_ga)
 
     def getelement(self, element):
-        elements = self.responsexml.xpath("//" + element)
+        elements = self.responsexml.xpath('//' + element)
         if elements and len(elements) > 0:
-            return self.responsexml.xpath("//" + element)[0]
+            return self.responsexml.xpath('//' + element)[0]
 
     def getelementattribute(self, element, attr):
         if element is not None:
@@ -24,20 +64,13 @@ class LogainmParser:
             return ''
 
     def getallelements(self, element):
-        return self.responsexml.xpath("//" + element)
+        return self.responsexml.xpath('//' + element)
 
-    def exists(self):
-        exists = self.getplace().get('nonexistent')
-        if exists == 'yes':
-            return False
-        else:
-            return True
+    def placeexists(self):
+        places = self.getallelements('place')
+        if places is not None and len(places) > 0:
+            nonexistent = places[0].get('nonexistent')
+            if nonexistent == 'yes':
+                return False
+        return True
 
-    def get_main_name(self, lang):
-        for name in self.responsexml.xpath("//name[@lang='" + lang + "']"):
-            if name.get('isMain') == 'yes':
-                wording = name.get('wording')
-                if not wording:
-                    wording = ''
-                return wording
-        return ''
